c4fe4e52b39915f2e9d36b8e4a1495b6819e9059
DEIVA SIVAM <115927155+asdeiva@users.noreply.github.com>
2022-12-30 15:32:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function oksubmit() {
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let email = document.getElementById("email").value;
+    let reason = document.getElementById("reason").value;
+    console.log(fname)
+    if(fname != "" && lname != "" && email != "" && reason != ""){
+        alert("Form submitted successfully")
+    }
+    else{
+        alert("All fileds are required")
+    }
+  }
